51259538120ec42678e083d1ad544b2fc4b6e34e
Unknown <mforall29@gmail.com>
2018-04-21 12:04:33 +0300

    + Pods

--- EZHack/EZHack/Presentation/Feed/FeedController.swift
+++ EZHack/EZHack/Presentation/Feed/FeedController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Evgeniy. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 final class FeedController: UIViewController {
@@ -20,6 +21,10 @@ final class FeedController: UIViewController {
         setupScreen()
     }
     
+    // MARK: - Members
+    
+    private let locator = LocationManager()
+    
     // MARK: - Methods
     
     private func setupScreen() {
@@ -27,10 +32,29 @@ final class FeedController: UIViewController {
     }
     
     private func setup() {
-        let setup = [setColors]
+        let setup = [setColors, testMyLocation]
         setup.forEach { $0() }
     }
     
     private func setColors() {
+    }
+    
+    private func testMyLocation() {
+        locator.checkRights()
+        locator.startUpdating()
+        locator.stopUpdating()
+        
+        locator.getCurrentLocation()
+            .then(printMyLocation)
+    }
+    
+    private func printMyLocation(_ location: CLLocation) {
+        log.debug("got location - \(location)")
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func retestLocation(_ sender: UIButton) {
+        testMyLocation()
     }
 }
