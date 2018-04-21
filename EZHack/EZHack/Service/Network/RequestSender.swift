534313b1b4c4edf2c7d72c90b858a76032dde76f
Unknown <mforall29@gmail.com>
2018-04-21 12:19:18 +0300

    Alamofire

---
+++ EZHack/EZHack/Service/Network/RequestSender.swift
@@ -0,0 +1,23 @@
+//
+//  RequestSender.swift
+//  EZHack
+//
+//  Created by supreme on 21/04/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+public final class RequestSender {
+
+    // MARK: - Members
+    
+    public static let shared = RequestSender()
+    
+    // MARK: - Interface
+    
+    public func get(_ url: String) {
+        
+    }
+}
---
+++ EZHack/EZHack/Service/Place/PlaceProvider.swift
@@ -0,0 +1,21 @@
+// 
+//  PlaceProvider.swift
+//  EZHack
+//
+//  Created by supreme on 21/04/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+public final class PlaceProvider {
+// MARK: - Members
+    public static let shared = PlaceProvider()
+    
+    // MARK: - Interface
+    
+    public func get(by location: CLLocationCoordinate2D) {
+        
+    }
+}
